fe038bf7c46c5cdca43abcf598971f93dd4012fd
RaoudhaHamdi <raoudha2017@outlook.fr>
2019-04-15 11:44:43 +0200

    add remoteMethod getEventsProfil && getProfilsEvent for both Event and Profil Models

--- common/models/event.js
+++ common/models/event.js
@@ -1,5 +1,25 @@
 'use strict';
-
+const app=require('../../server/server')
 module.exports = function(Event) {
+   
+
+Event.getEventsProfil= (id,next) =>{
+const eventsProfil=[]
+const Part=app.models.Part
+    Part.find({"include":["event"]},(err,res)=>{
+        if(err){next(err)}
+        for(var i=0;i<res.length;i++){
+            if(res[i].profilId==id)
+            eventsProfil.push(res[i])
+        }
+        next(null,eventsProfil)
+    })
+}
+Event.remoteMethod('getEventsProfil',{
+description:"get list of events per Profil #####",
+accepts:{arg:'id','type':'string'},
+returns:{arg:'result'},
+http:{verb:'get',path:'/getEventsProfil'}
+})
 
 };
--- common/models/profil.js
+++ common/models/profil.js
@@ -1,5 +1,23 @@
 'use strict';
 const app=require('../../server/server')
 module.exports = function(Profil) {
+Profil.getProfilsEvent= (id,next) =>{
+const ProfilsEvent=[]
+const Part=app.models.Part
+    Part.find({"include":["profil"]},(err,res)=>{
+        if(err){next(err)}
+        for(var i=0;i<res.length;i++){
+            if(res[i].eventId==id)
+            ProfilsEvent.push(res[i])
+        }
+        next(null,ProfilsEvent)
+    })
+}
+Profil.remoteMethod('getProfilsEvent',{
+description:"get list of profils per Event #####",
+accepts:{arg:'id','type':'string'},
+returns:{arg:'result'},
+http:{verb:'get',path:'/getProfilsEvent'}
+})
 
 };
